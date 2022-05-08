7299a979353a6c32d06aabbf64e92c1d588fdc38
fangqk1991 <me@fangqk.com>
2022-05-08 13:52:17 +0800

    v1.2.0: +AppException

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,39 @@
-export default class AppError extends Error {
+export default class AppError<T = any> extends Error {
   public name: string
   public statusCode: number
-  public extras: any
+  public extras: T
 
   constructor(message: string, statusCode = 500, extras: any = {}) {
     super(message)
     this.name = 'AppError'
     this.statusCode = statusCode
     this.extras = extras
+  }
+}
+
+export interface ErrorModel {
+  phrase: string
+  message?: string
+  statusCode?: number
+}
+
+export class AppException extends Error {
+  public name: string
+  public statusCode: number
+  public errorBody: ErrorModel
+
+  constructor(errorBody: ErrorModel, statusCode = 400) {
+    super(JSON.stringify(errorBody))
+    this.errorBody = errorBody
+    this.name = 'AppException'
+    this.statusCode = statusCode
+  }
+
+  public static exception(phrase: string, params: Partial<ErrorModel> = {}) {
+    const errorBody: ErrorModel = {
+      ...params,
+      phrase: phrase,
+    }
+    return new AppException(errorBody, params.statusCode || 400)
   }
 }
